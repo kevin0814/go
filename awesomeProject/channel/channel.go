ee50aea3c150850fee541bb3601d2d8febe5e642
kevin0814 <33565082+kevin0814@users.noreply.github.com>
2021-06-01 14:16:12 +0800

    通道

--- awesomeProject/channel/channel.go
+++ awesomeProject/channel/channel.go
@@ -4,9 +4,9 @@ func main(){
 	var channel chan int = make(chan int)
 	// Go中通道可以是发送（send）、接收（receive）、同时发送（send）和接收（receive）。
 	// 定义接收的通道
-	//receive_only := make (<-chan int)
+	receive_only := make (<-chan int)
 	// 定义发送的通道
-	//send_only := make (chan<- int)
+	send_only := make (chan<- int)
 	// 可同时发送接收
 	//send_receive := make (chan int)
 	// chan<- 表示数据进入通道，要把数据写进通道，对于调用者就是发送。

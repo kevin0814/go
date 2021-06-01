960eea494e64b5c5d9c69bfbf06c75f5ba88ca0e
kevin0814 <33565082+kevin0814@users.noreply.github.com>
2021-06-01 17:08:59 +0800

    结构体练习

--- awesomeProject/struck.go
+++ awesomeProject/struck.go
@@ -8,17 +8,31 @@ import ("fmt"
 		Age int
 	}
 
+type person struct {
+	name string
+	city string
+	age  int8
+}
+
 	func main() {
+		p3 := &person{} //使用&对结构体进行取地址操作相当于对该结构体类型进行了一次new实例化操作
+		fmt.Printf("%T\n", p3)     //*main.person
+		fmt.Printf("p3=%#v\n", p3) //p3=&main.person{name:"", city:"", age:0}
+		p3.name = "博客"
+		p3.age = 30
+		p3.city = "成都"
+		fmt.Printf("p3=%#v\n", p3) //p3=&main.person{name:"博客", city:"成都", age:30}
+
 		var p1 Person
 		p1.Name = "Tom"
 		p1.Age  = 30
 		fmt.Println("p1 =", p1)
-               //结构体 赋值
+
 		var p2 = Person{Name:"Burke", Age:31}
 		fmt.Println("p2 =", p2)
 
-		p3 := Person{Name:"Aaron", Age:32}
-		fmt.Println("p2 =", p3)
+		//p3 := Person{Name:"Aaron", Age:32}
+		//fmt.Println("p2 =", p3)
 
 		//匿名结构体
 		p4 := struct {

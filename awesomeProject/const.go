d20298211c4475460769b8367f355e397c436bea
kevin0814 <33565082+kevin0814@users.noreply.github.com>
2021-06-01 14:10:37 +0800

    常量

--- awesomeProject/const.go
+++ awesomeProject/const.go
@@ -30,7 +30,7 @@ func main(){
    fmt.Println(ll)
 
 	const noDelay time.Duration = 0
-	const timeout = 5 * time.Minute
+	const timeout = 5 * time.Minute //时间
 	fmt.Printf("%T %[1]v\n", noDelay)     // "time.Duration 0"
 	fmt.Printf("%T %[1]v\n", timeout)     // "time.Duration 5m0s"
 	fmt.Printf("%T %[1]v\n", time.Minute) // "time.Duration 1m0s"

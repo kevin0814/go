952510f7d2ff3aa5972407a6681e62296b1d2033
kevin0814 <33565082+kevin0814@users.noreply.github.com>
2021-06-01 16:32:49 +0800

    指针取值和指针取内存

--- awesomeProject/zhizhen.go
+++ awesomeProject/zhizhen.go
@@ -15,7 +15,14 @@ func g() {
 }
 //变量的生命周期 在函数被调用的时候，创建，函数结束之后变量销毁
 func main(){
- 	fmt.Println("hello ,world")
+ 		//指针取值
+	a := 10
+	b := &a // 取变量a的地址，将指针保存到b中
+	fmt.Printf("type of b:%T\n", b)  //type of b:*int
+	c := *b // 指针取值（根据指针去内存取值）
+	fmt.Printf("type of c:%T\n", c)  //type of c:int
+	fmt.Printf("value of c:%v\n", c) //value of c:10
+	//PrintfIn
  }
 //变量的作用域 全局变量（在程序结束的时候销毁） 局部变量（在函数调用结束被销毁）
 // 堆和栈

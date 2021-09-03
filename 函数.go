961cf2e87e2c84e8e096448f868258db43529b58
kevin0814 <33565082+kevin0814@users.noreply.github.com>
2021-09-03 15:47:33 +0800

    Create 函数.go

---
+++ 函数.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	/* 定义局部变量 */
+	var a int = 100
+	var b int = 200
+	var ret int
+
+	/* 调用函数并返回最大值 */
+	ret = max(a, b)
+
+	fmt.Printf( "最大值是 : %d\n", ret )
+
+	/* 声明函数变量 */
+	getSquareRoot := func(x float64) float64 {
+		return math.Sqrt(x)
+	}
+
+	/* 使用函数 */
+	fmt.Println(getSquareRoot(9))
+}
+
+/* 函数返回两个数的最大值 */
+func max(num1, num2 int) int {
+	/* 定义局部变量 */
+	var result int
+
+	if (num1 > num2) {
+		result = num1
+	} else {
+		result = num2
+	}
+	return result
+
+
+}

1dee304dfbf7ccb20882019c94a3612aa10a29a6
kevin0814 <33565082+kevin0814@users.noreply.github.com>
2021-09-03 16:15:21 +0800

    Create timer_ticker.go

---
+++ 定时器/timer_ticker.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+"fmt"
+"time"
+)
+//Ticker：时间到了，多次执行
+func main() {
+	// 1.获取ticker对象
+	ticker := time.NewTicker(1 * time.Second)
+	i := 0
+	// 子协程
+	go func() {
+		for {
+			//<-ticker.C
+			i++
+			fmt.Println(<-ticker.C)
+			if i == 5 {
+				//停止
+				ticker.Stop()
+			}
+		}
+	}()
+	for {
+	}
+}
